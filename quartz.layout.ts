8fabd1e6d7fe99e1689b0ade6f993714ae646486
acubed3 <aleksandrov.aa@phystech.edu>
2024-09-16 16:48:58 +0300

    Quartz sync: Sep 16, 2024, 4:48 PM

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -27,10 +27,10 @@ export const defaultContentPageLayout: PageLayout = {
     Component.MobileOnly(Component.Spacer()),
     //Component.Search(),
     Component.Darkmode(),
-    Component.DesktopOnly(Component.Explorer())
+    Component.DesktopOnly()
   ],
   right: [
-    Component.Graph(),
+    //Component.Graph(),
     //Component.DesktopOnly(Component.TableOfContents()),
     //Component.Backlinks(),
   ],
@@ -44,7 +44,7 @@ export const defaultListPageLayout: PageLayout = {
     Component.MobileOnly(Component.Spacer()),
     Component.Search(),
     Component.Darkmode(),
-    Component.DesktopOnly(Component.Explorer()),
+    Component.DesktopOnly(),
   ],
   right: [],
 }
